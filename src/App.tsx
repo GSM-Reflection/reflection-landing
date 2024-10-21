9b1e23bbbe6b58c17d27471fed3a1429b357892a
hyojoo <jhj080802@naver.com>
2024-10-21 10:50:12 +0900

    init: 초기화

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
+import Background from "./components/BackGround";
+import Question from "./components/Question";
+import Timetable from "./components/Timetable";
 import Title from "./components/Title";
 
 function App() {
   return (
     <>
       <Title></Title>
+      <Background />
+      <Timetable />
+      <Question />
     </>
   );
 }
