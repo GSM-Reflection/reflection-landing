c2fe37d59fefed5c598f0fe3595e245ab55ab21a
hyojoo <jhj080802@naver.com>
2024-10-21 15:09:14 +0900

    feat: faq기능

--- src/components/Question.tsx
+++ src/components/Question.tsx
@@ -1,3 +1,56 @@
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+
 export default function Question() {
-  return <div>question</div>;
+  const faqs = [
+    {
+      question: "행사는 어디서 진행을 하나요?",
+      answer: "1",
+    },
+    {
+      question: "행사는 어디서 진행을 하나요?",
+      answer:
+        "행사는 광주 소프트웨어 마이스터고 금봉관 (강당)에서 진행이 됩니다!",
+    },
+    {
+      question: "행사는 어디서 진행을 하나요?",
+      answer: "3",
+    },
+    {
+      question: "행사는 어디서 진행을 하나요?",
+      answer: "4",
+    },
+  ];
+  const onClick = () => {
+    window.location.href = "https://www.instagram.com/reflection_conf/";
+  };
+  return (
+    <div className="flex flex-col w-full mb-[5rem] justify-center items-center">
+      <h1 className="mb-[5rem]">많이 들어오는 질문</h1>
+
+      <div className="w-full max-w-[1100px] mb-10 px-4">
+        <Accordion className="flex flex-col gap-4" type="single" collapsible>
+          {faqs.map((faq, index) => (
+            <AccordionItem key={index} value={index.toString()}>
+              <AccordionTrigger>Q. {faq.question}</AccordionTrigger>
+              <AccordionContent>A. {faq.answer}</AccordionContent>
+            </AccordionItem>
+          ))}
+        </Accordion>
+      </div>
+
+      <div className="w-full flex flex-col md:flex-row justify-center items-center max-w-md mt-4">
+        <p className="text-sm font-light md:mr-2 mb-2 md:mb-0">
+          더 궁금한 점이 있다면?
+        </p>
+        <p onClick={onClick} className="font-semibold cursor-pointer">
+          담당자에게 연락하기
+        </p>
+      </div>
+    </div>
+  );
 }
