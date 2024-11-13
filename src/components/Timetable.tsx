681c7cc8b498ba42582b703793f37c8f5d7f2753
h-0y28 <s24015@gsm.hs.kr>
2024-11-13 17:21:22 +0900

    style: 코드 스타일 변경

--- src/components/Timetable.tsx
+++ src/components/Timetable.tsx
@@ -5,16 +5,25 @@ import {
 } from "@/components/ui/accordion";
 import ConferenceSession from "./conferenceSession";
 
+const sessionData = [
+  { speaker: "유태관 주무관 1", topic: "주제 쓰기 1", time: "13:30 ~ 13:50" },
+  { speaker: "유태관 주무관 2", topic: "주제 쓰기 2", time: "13:30 ~ 13:50" },
+  { speaker: "유태관 주무관 3", topic: "주제 쓰기 3", time: "13:30 ~ 13:50" },
+  { speaker: "유태관 주무관 4", topic: "주제 쓰기 4", time: "13:30 ~ 13:50" },
+  { speaker: "유태관 주무관 5", topic: "주제 쓰기 5", time: "13:30 ~ 13:50" },
+];
+
 export default function Timetable() {
   return (
-    <div className="flex justify-center mb-[6rem]">
-      <div className="w-2/3">
+    <div className="flex justify-center mb-[6rem] px-4 sm:px-10 lg:px-12">
+      <div className="w-full max-w-screen-lg">
         <p className="mb-[5rem] text-[1.5rem] text-center font-light">
           섹션 시간표
         </p>
+
         {/* 개회식 */}
         <p className="text-[#A1A1A1] text-[1.25rem] mb-6">13:00 ~ 13:30</p>
-        <div className="flex items-center gap-4 mb-[2rem]">
+        <div className="flex flex-col sm:flex-row items-center gap-4 mb-[2rem]">
           <p className="text-[2rem]">개회식</p>
           <hr className="flex-grow border-t border-[#6E6E6E]" />
           <p className="text-[2rem]">개회식때 무엇을 할건지 간단히 설명</p>
@@ -28,63 +37,25 @@ export default function Timetable() {
               <ConferenceSession />
 
               {/* 컨퍼런스 내용 */}
-              <AccordionContent className="pr-6">
-                <p className="text-[#A1A1A1] text-[1.25rem] mb-6">
-                  13:30 ~ 13:50
-                </p>
-                <div className="flex items-center gap-4 ">
-                  <p className="text-[2rem]">유태관 주무관</p>
-                  <hr className="flex-grow border-t border-[#6E6E6E]" />
-                  <p className="text-[2rem]">주제 쓰기</p>
-                </div>
-              </AccordionContent>
-              <AccordionContent className="pr-6">
-                <p className="text-[#A1A1A1] text-[1.25rem] mb-6">
-                  13:30 ~ 13:50
-                </p>
-                <div className="flex items-center gap-4 ">
-                  <p className="text-[2rem]">유태관 주무관</p>
-                  <hr className="flex-grow border-t border-[#6E6E6E]" />
-                  <p className="text-[2rem]">주제 쓰기</p>
-                </div>
-              </AccordionContent>
-              <AccordionContent className="pr-6">
-                <p className="text-[#A1A1A1] text-[1.25rem] mb-6">
-                  13:30 ~ 13:50
-                </p>
-                <div className="flex items-center gap-4 ">
-                  <p className="text-[2rem]">유태관 주무관</p>
-                  <hr className="flex-grow border-t border-[#6E6E6E]" />
-                  <p className="text-[2rem]">주제 쓰기</p>
-                </div>
-              </AccordionContent>
-              <AccordionContent className="pr-6">
-                <p className="text-[#A1A1A1] text-[1.25rem] mb-6">
-                  13:30 ~ 13:50
-                </p>
-                <div className="flex items-center gap-4 ">
-                  <p className="text-[2rem]">유태관 주무관</p>
-                  <hr className="flex-grow border-t border-[#6E6E6E]" />
-                  <p className="text-[2rem]">주제 쓰기</p>
-                </div>
-              </AccordionContent>
-              <AccordionContent className="pr-6">
-                <p className="text-[#A1A1A1] text-[1.25rem] mb-6">
-                  13:30 ~ 13:50
-                </p>
-                <div className="flex items-center gap-4 mb-[2rem]">
-                  <p className="text-[2rem]">유태관 주무관</p>
-                  <hr className="flex-grow border-t border-[#6E6E6E]" />
-                  <p className="text-[2rem]">주제 쓰기</p>
-                </div>
-              </AccordionContent>
+              {sessionData.map((session, index) => (
+                <AccordionContent className="pr-6" key={index}>
+                  <p className="text-[#A1A1A1] text-[1.25rem] mb-6">
+                    {session.time}
+                  </p>
+                  <div className="flex flex-col sm:flex-row items-center gap-4">
+                    <p className="text-[2rem]">{session.speaker}</p>
+                    <hr className="flex-grow border-t border-[#6E6E6E]" />
+                    <p className="text-[2rem]">{session.topic}</p>
+                  </div>
+                </AccordionContent>
+              ))}
             </AccordionItem>
           </Accordion>
         </div>
 
         {/* 기수별 대항전 */}
         <p className="text-[#A1A1A1] text-[1.25rem] mb-6">16:00 ~ 17:00</p>
-        <div className="flex items-center gap-4 mb-[2rem]">
+        <div className="flex flex-col sm:flex-row items-center gap-4 mb-[2rem]">
           <p className="text-[2rem]">기수별 대항전</p>
           <hr className="flex-grow border-t border-[#6E6E6E]" />
           <p className="text-[2rem]">
@@ -94,7 +65,7 @@ export default function Timetable() {
 
         {/* 네트워킹 */}
         <p className="text-[#A1A1A1] text-[1.25rem] mb-6">16:00 ~ 17:00</p>
-        <div className="flex items-center gap-4 mb-[2rem]">
+        <div className="flex flex-col sm:flex-row items-center gap-4 mb-[2rem]">
           <p className="text-[2rem]">네트워킹</p>
           <hr className="flex-grow border-t border-[#6E6E6E]" />
           <p className="text-[2rem]">
