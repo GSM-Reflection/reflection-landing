14020aea40fa6e770e9bfe1018a66726ed8bdec5
h-0y28 <s24015@gsm.hs.kr>
2024-11-13 16:49:14 +0900

    style: increase font size

--- src/components/conferenceSession.tsx
+++ src/components/conferenceSession.tsx
@@ -6,14 +6,14 @@ export default function ConferenceSession() {
     <>
       <AccordionTrigger className="p-0 w-full transition-all  [&[data-state=open]>svg]:rotate-180 relative">
         <div className="flex items-center gap-4 w-full">
-          <p className="text-lg ">컨퍼런스</p>
+          <p className="text-[2rem] ">컨퍼런스</p>
           <hr className="flex-grow border-t border-[#6E6E6E] h-[1px] " />
-          <p className="text-lg ">학교를 졸업하신 선배들과의 만남</p>
+          <p className="text-[2rem] ">학교를 졸업하신 선배들과의 만남</p>
         </div>
         <ChevronDown className="h-6 w-6 shrink-0 transition-transform duration-200 mr-4 absolute -right-11 text-[#6E6E6E]" />
       </AccordionTrigger>
 
-      <p className=" text-[#6E6E6E] text-sm text-right mb-[2rem]">
+      <p className=" text-[#6E6E6E] text-[1.25rem] mt-5 text-right mb-[2rem]">
         컨퍼런스는 20분 발표 후 10분 휴식으로 진행이 됩니다.
       </p>
     </>
