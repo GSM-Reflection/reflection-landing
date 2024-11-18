04719535a65517c225dc4413e4accafa92198869
h-0y28 <s24015@gsm.hs.kr>
2024-11-18 19:45:03 +0900

    style: add text font

--- src/components/Reserve.tsx
+++ src/components/Reserve.tsx
@@ -10,7 +10,7 @@ export default function Reserve() {
       <div className="mt-[458px]">
         <div className="mb-[238px]">
           <img src={reflectionText} alt="" className="mb-[16px]" />
-          <div className="flex flex-col md:flex-row justify-between text-[40px]">
+          <div className="flex flex-col md:flex-row justify-between text-[40px] font-pen">
             <p>Those of us who</p>
             <p className="text-end">came back to school</p>
           </div>
