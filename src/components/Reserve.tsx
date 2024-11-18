6f39635067090502287381bd5d8a3656bba10e26
h-0y28 <s24015@gsm.hs.kr>
2024-11-18 19:57:51 +0900

    style: 스타일 수정

--- src/components/Reserve.tsx
+++ src/components/Reserve.tsx
@@ -7,8 +7,8 @@ export default function Reserve() {
       className="flex w-full h-dvh justify-center bg-cover bg-center"
       style={{ backgroundImage: `url('/src/assets/bg.svg')` }}
     >
-      <div className="mt-[458px]">
-        <div className="mb-[238px]">
+      <div className="mt-[458px] md:mt-[400px] px-4 sm:px-8 md:px-16 lg:px-32">
+        <div className="mb-[130px] lg:mb-[238px]">
           <img src={reflectionText} alt="" className="mb-[16px]" />
           <div className="flex flex-col md:flex-row justify-between text-[40px] font-pen">
             <p>Those of us who</p>
@@ -16,7 +16,9 @@ export default function Reserve() {
           </div>
         </div>
         <div className="flex justify-center">
-          <ReserveButton />
+          <div className="max-w-full w-full flex justify-center">
+            <ReserveButton />
+          </div>
         </div>
       </div>
     </div>
